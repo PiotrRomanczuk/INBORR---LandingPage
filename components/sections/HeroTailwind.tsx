c128656b61597afb9d1c8ebfe9bbdf294fb66fef
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-12-16 21:43:06 +0100

    cors headers for route api

--- components/sections/HeroTailwind.tsx
+++ components/sections/HeroTailwind.tsx
@@ -21,8 +21,8 @@ export const HeroTailwind = () => {
             </svg>
             <div className="relative px-6 py-20 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
               <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
-                <div className="hidden sm:mb-10 sm:flex"></div>
-                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                <div className=" sm:mb-10 sm:flex"></div>
+                <h1 className=" text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                   <TextAnimation />
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
--- components/ui/TextAnimation.tsx
+++ components/ui/TextAnimation.tsx
@@ -2,30 +2,31 @@ import { TypeAnimation } from "react-type-animation";
 
 export const TextAnimation = () => {
   return (
-    <TypeAnimation
-      sequence={[
-        "Twój dom na chwilę",
-        //    - komfort i przyjemność w sercu Warszawy!
-        1000,
-        "Warszawska chwila luksusu",
-        //  - wynajmij z nami!
-        1000,
-        "Twoje miejsce w Warszawie",
-        //  - z nami zawsze blisko wszystkiego!
-        1000,
-        "Z nami zawsze z poczuciem domu",
-        //  - !
-        1000,
-        "Wynajmij komfort w Warszawie",
-        //  - z nami każdy moment staje się wyjątkowy!
-        1000,
-      ]}
-      //  Pull request genereator, I'm a pull request hand;es
-      speed={30}
-      className="min-h-[3.5rem] text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:text-5xl"
-      repeat={Infinity}
-      preRenderFirstString
-    />
+    <div className="h-32">
+      <TypeAnimation
+        sequence={[
+          "Twój dom na chwilę",
+          //    - komfort i przyjemność w sercu Warszawy!
+          1000,
+          `Warszawska chwila luksusu`,
+          //  - wynajmij z nami!
+          1000,
+          "Twoje miejsce w Warszawie",
+          //  - z nami zawsze blisko wszystkiego!
+          1000,
+          "Z nami zawsze z poczuciem domu",
+          //  - !
+          1000,
+          "Wynajmij komfort w Warszawie",
+          //  - z nami każdy moment staje się wyjątkowy!
+          1000,
+        ]}
+        //  Pull request genereator, I'm a pull request hand;es
+        speed={30}
+        className="min-h-[3.5rem] text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:text-5xl"
+        repeat={Infinity}
+        preRenderFirstString
+      />
+    </div>
   );
 };
-
